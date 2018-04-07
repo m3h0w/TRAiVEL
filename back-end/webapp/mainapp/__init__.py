70e07da40accb285075e9d66f2cc6aec50f89f69
Michal <michal@tiatechnology.com>
2018-04-07 21:33:04 +0200

    Add mockup tweets

--- back-end/webapp/mainapp/__init__.py
+++ back-end/webapp/mainapp/__init__.py
@@ -3,3 +3,14 @@ from flask import Flask
 from flask import jsonify, request
 
 app = Flask(__name__, static_url_path='/static')
+
+@app.route("/get_data", methods=['GET'])
+def display_sentiment():
+  import json
+  from pprint import pprint
+
+  data = json.load(open('data.json'))
+
+  pprint(data)
+
+  return jsonify(get_sentiment_list(strings_list))
