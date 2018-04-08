b356c53c90777214bb2e329f2e8ffc170e428e0f
Michal <michal@tiatechnology.com>
2018-04-08 05:57:52 +0200

    shit

--- back-end/skyscan/mainapp/__init__.py
+++ back-end/skyscan/mainapp/__init__.py
@@ -2,22 +2,24 @@
 from flask import Flask
 from flask import jsonify, request
 
-from prices import get_prices_from_cities
+from prices import get_prices_from_cities, get_next_friday_sunday
 
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='/static')
 # app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
-print("CORS ADDED")
 
 @app.route("/get_prices", methods=['POST'])
 def get_prices():
   if 'cities_list' in request.json and isinstance(request.json['cities_list'], list):
     strings_list = request.json['cities_list']
-  
-  month = "2018-04"
-  if 'month' in request.json and isinstance(request.json['month'], str):
-    month = request.json['month']
 
-  return jsonify(get_prices_from_cities(strings_list, start_month=month, end_month=month))
+  start,end = get_next_friday_sunday()
+  data = get_prices_from_cities(strings_list,start,end,origin_city='Copenhagen',currency='EUR')
+  
+  # month = "2018-04"
+  # if 'month' in request.json and isinstance(request.json['month'], str):
+  #   month = request.json['month']
+
+  return jsonify(data)
--- back-end/webapp/mainapp/__init__.py
+++ back-end/webapp/mainapp/__init__.py
@@ -9,16 +9,37 @@ import numpy as np
 
 from flask_cors import CORS
 
+from geopy.geocoders import Nominatim
+from requests import async
+
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
 
+def lat_long(city):
+  country = country_dic[city]
+  loc = city+', '+country
+
+  geolocator = Nominatim()
+  location = geolocator.geocode(loc)
+  return {"latitude": location.latitude, "longitude": location.longitude}
+
+def get_lat_longs(city_list):
+  loc_dict = {}
+  for i in city_list:
+    loc = lat_long(city)
+    loc_dict[i] = loc
+  return loc_dict
+
+country_dic = {'Copenhagen': 'Denmark', 'London': 'England','Berlin':'Germany','Amsterdam':'Nederland','Paris':'France','Warsaw':'Poland', 'Moscow': 'Russia'}
+lang_dic = {'Copenhagen':'da','London':'en','Berlin':'de','Amsterdam':'nl','Paris':'fr','Warsaw':'pl', 'Russia':'ru'}
+
 @app.route("/get_data", methods=['GET'])
 def get_data():
-  cities = ["Warsaw", "Madrid"]
-  countries = ["POL", "ESP"]
+  cities = ['Amsterdam','Berlin','Warsaw','Moscow']
+  countries = ['NLD', 'DEU', 'POL', 'RUS']
+  # countries = ["POL", "ESP"]
   sentiments = get_sentiment(cities)
-  flights_raw = get_flights(cities, "2018-04")
-  
+  flights_raw = get_flights(cities)
   
   data = {}
   for index, country in enumerate(countries):
@@ -28,9 +49,12 @@ def get_data():
       "sentiment": sentiments[index], 
       "flight": {
         "price": flight["Price"],
-        "data": flight["InboundLegDepart"],
-        "airlineLogo": flight["InboundCarrier"] + ".png"
-      }
+        "outdate": flight["OutboundLegDepart"],
+        "indate": flight["InboundLegDepart"],
+        "airline": flight["InboundCarrier"]
+      },
+      "link": flight['url'],
+      "location": lat_long(cities[index])      
     } 
 
   # print(data)
@@ -64,9 +88,9 @@ def get_sentiment(cities):
   payload = {"text_list": the_list}
   r = requests.post('http://127.0.0.1:8002/get_sentiment', json = payload)
   json_data = json.loads(r.text)
-  return [np.mean(json_data), np.mean(json_data) - 0.2]
+  return [np.mean(json_data), np.mean(json_data) - 0.2, np.mean(json_data) + 0.1, np.mean(json_data) + 0.05, np.mean(json_data)]
 
-def get_flights(cities, month):
+def get_flights(cities):
   payload = {"cities_list": cities}
   r = requests.post('http://127.0.0.1:8003/get_prices', json = payload)
   json_data = json.loads(r.text)
